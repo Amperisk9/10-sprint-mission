5d33befd7e54135b936a1a68d8200cf3c87cbfeb
Amperisk9 <amperisk9@gmail.com>
2026-02-23 09:48:36 +0900

    fix(exception): exception관련 코드 개선

--- src/main/java/com/sprint/mission/discodeit/exception/ErrorCode.java
+++ src/main/java/com/sprint/mission/discodeit/exception/ErrorCode.java
@@ -3,7 +3,7 @@ package com.sprint.mission.discodeit.exception;
 import lombok.Getter;
 
 @Getter
-public enum BusinessExceptionCode {
+public enum ErrorCode {
     // User
     USER_NOT_FOUND(404, "사용자를 찾을 수 없음"),
     USER_WRONG_PASSWORD(400, "계정명이나 패스워드가 다릅니다");
@@ -11,8 +11,12 @@ public enum BusinessExceptionCode {
     private final int status;
     private final String message;
 
-    BusinessExceptionCode(int status, String message) {
+    ErrorCode(int status, String message) {
         this.status = status;
         this.message = message;
+    }
+
+    public String getCode() {
+        return this.name();
     }
 }
--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.sprint.mission.discodeit.exception;
 
-import org.springframework.http.HttpStatus;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,25 +8,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<ErrorResponse> handleException(Exception e) {
-//        ErrorResponse response = new ErrorResponse(33, e.getMessage());
-//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
-//    }
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ErrorResponse errorResponse = ErrorResponse.of(
+                400, "VALIDATION_ERROR", "요청 값이 올바르지 않습니다.", e.getBindingResult());
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleDTOException(MethodArgumentNotValidException e) {
-        ErrorResponse response = new ErrorResponse(e);
-        System.out.printf("메시지: %s%n", e.getMessage());
-        System.out.printf("body: %s%n", e.getBody());
-        System.out.printf("DetailMessageArguments: %s%n", e.getDetailMessageArguments());
-        System.out.printf("getParameter: %s%n", e.getParameter());
-        System.out.printf("getStatusCode: %s%n", e.getStatusCode());
-        System.out.printf("getCause: %s%n", e.getCause());
-        System.out.printf("getLocalizedMessage: %s%n", e.getLocalizedMessage());
-        System.out.printf("getBindingResult: %s%n", e.getBindingResult());
-        e.getBindingResult().
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException e) {
+        ErrorResponse errorResponse = ErrorResponse.of(
+                400, "VALIDATION_ERROR", "요청 값이 올바르지 않습니다.", e.getConstraintViolations());
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    }
 
-        return ResponseEntity.status(402).body(response);
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleBusinessLogicException(BusinessLogicException e) {
+        ErrorResponse errorResponse = ErrorResponse.of(e.getStatus(), e.getCode(), e.getMessage());
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
     }
 }
