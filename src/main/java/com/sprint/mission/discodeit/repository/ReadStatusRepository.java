720996ee8ee95413e5736a69268a5374c43d2e44
Amperisk9 <amperisk9@gmail.com>
2026-03-05 18:21:32 +0900

    Repository 구현체 Jcf, File 제거 및 Repository 인터페이스에 JpaRepository 상속 추가

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -1,16 +1,12 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.ReadStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
-public interface ReadStatusRepository {
-    ReadStatus save(ReadStatus readStatus);
-    Optional<ReadStatus> findById(UUID uuid);
-    List<ReadStatus> findAll();
+public interface ReadStatusRepository extends JpaRepository<ReadStatus, UUID> {
     List<ReadStatus> findAllByUserId(UUID userId);
-    void deleteById(UUID uuid);
     void deleteAllByChannelId(UUID channelId);
 }
