a02a21b298f8695ae39a928a7ad2d5e37012c9f1
Amperisk9 <amperisk9@gmail.com>
2026-03-27 03:06:26 +0900

    feat: exception 개선

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -15,23 +15,25 @@ public class GlobalExceptionHandler {
 //        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
 //    }
 
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        ErrorResponse errorResponse = ErrorResponse.of(
-                400, "VALIDATION_ERROR", "요청 값이 올바르지 않습니다.", e.getBindingResult());
-        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
-    }
+  @ExceptionHandler
+  public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+      MethodArgumentNotValidException e) {
+    ErrorResponse errorResponse = ErrorResponse.of(
+        400, e.getClass().getName(), e.getBindingResult());
+    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+  }
 
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException e) {
-        ErrorResponse errorResponse = ErrorResponse.of(
-                400, "VALIDATION_ERROR", "요청 값이 올바르지 않습니다.", e.getConstraintViolations());
-        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
-    }
+  @ExceptionHandler
+  public ResponseEntity<ErrorResponse> handleConstraintViolationException(
+      ConstraintViolationException e) {
+    ErrorResponse errorResponse = ErrorResponse.of(
+        400, e.getClass().getName(), e.getConstraintViolations());
+    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+  }
 
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleBusinessLogicException(BusinessLogicException e) {
-        ErrorResponse errorResponse = ErrorResponse.of(e.getStatus(), e.getCode(), e.getMessage());
-        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
-    }
+  @ExceptionHandler
+  public ResponseEntity<ErrorResponse> handleBusinessLogicException(DiscodeitException e) {
+    ErrorResponse errorResponse = ErrorResponse.of(400, e);
+    return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+  }
 }
