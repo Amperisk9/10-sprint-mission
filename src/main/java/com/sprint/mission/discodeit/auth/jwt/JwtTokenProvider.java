9d3821ef7f9b5e9e4b67cb513dbed7b504acdfab
Amperisk9 <amperisk9@gmail.com>
2026-05-22 17:34:31 +0900

    feat: JWT 컴포넌트 구현

---
+++ src/main/java/com/sprint/mission/discodeit/auth/jwt/JwtTokenProvider.java
@@ -0,0 +1,93 @@
+package com.sprint.mission.discodeit.auth.jwt;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.JWTClaimsSet.Builder;
+import com.nimbusds.jwt.SignedJWT;
+import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Map;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JwtTokenProvider {
+
+  @Getter
+  @Value("${discodeit.jwt.key}")
+  private String secretKey;
+
+  @Getter
+  @Value("${discodeit.jwt.access-token-expiration-minutes}")
+  private long accessTokenExpirationMinutes;
+
+  @Getter
+  @Value("${discodeit.jwt.refresh-token-expiration-minutes}")
+  private long refreshTokenExpirationMinutes;
+
+  public String generateAccessToken(Map<String, Object> claims, String subject) {
+    return generateToken(TokenType.ACCESS, claims, subject);
+  }
+
+  public String generateRefreshToken(String subject) {
+    return generateToken(TokenType.REFRESH, null, subject);
+  }
+
+  private String generateToken(TokenType tokenType, Map<String, Object> claims, String subject) {
+    try {
+      // KeyLengthException
+      JWSSigner signer = new MACSigner(secretKey.getBytes(StandardCharsets.UTF_8));
+      JWTClaimsSet claimsSet = switch (tokenType) {
+        case ACCESS -> new Builder()
+            .subject(subject)
+            .issueTime(new Date())
+            .expirationTime(
+                new Date(System.currentTimeMillis() + accessTokenExpirationMinutes * 60 * 1000))
+            .claim("roles", claims.get("roles"))
+            .build();
+        case REFRESH -> new Builder()
+            .subject(subject)
+            .issueTime(new Date())
+            .expirationTime(
+                new Date(System.currentTimeMillis() + refreshTokenExpirationMinutes * 60 * 1000))
+            .build();
+      };
+      SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
+      // JOSEException
+      signedJWT.sign(signer);
+      return signedJWT.serialize();
+    } catch (JOSEException e) {
+      throw new RuntimeException("JWT 발급 실패", e);
+    }
+  }
+
+  enum TokenType {
+    ACCESS,
+    REFRESH
+  }
+
+  public Map<String, Object> getClaims(String token) {
+    try {
+      SignedJWT signedJWT = SignedJWT.parse(token);
+      JWSVerifier verifier = new MACVerifier(secretKey.getBytes(StandardCharsets.UTF_8));
+
+      if (!signedJWT.verify(verifier)) {
+        throw new RuntimeException("JWT 검증 실패");
+      }
+
+      return signedJWT.getJWTClaimsSet().getClaims();
+    } catch (ParseException | JOSEException e) {
+      throw new RuntimeException("잘못된 형식의 토큰", e);
+    }
+  }
+}
